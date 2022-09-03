717abb716af594cfbfa6b119bdc322a28817e39d
oldalpinist <112462171+oldalpinist@users.noreply.github.com>
2022-09-03 22:08:29 +0800

    n программистов
    В институте биоинформатики по офису передвигается робот. Недавно студенты из группы программистов написали для него программу, по которой робот, когда заходит в комнату, считает количество программистов в ней и произносит его вслух: "n программистов".
    
    Для того, чтобы это звучало правильно, для каждого nn нужно использовать верное окончание слова.
    
    Напишите программу, считывающую с пользовательского ввода целое число nn (неотрицательное), выводящее это число в консоль вместе с правильным образом изменённым словом "программист", для того, чтобы робот мог нормально общаться с людьми, например: 1 программист, 2 программиста, 5 программистов.
    
    В комнате может быть очень много программистов. Проверьте, что ваша программа правильно обработает все случаи, как минимум до 1000 человек.
    
    Дополнительный комментарий к условию:
    Обратите внимание, что задача не так проста, как кажется на первый взгляд. Если ваше решение не проходит какой-то тест, это значит, что вы не рассмотрели какой-то из случаев входных данных (число программистов 0 \le n \le 10000≤n≤1000). Обязательно проверяйте свои решения на дополнительных значениях, а не только на тех, что приведены в условии задания.

---
+++ Case-programisti.py
@@ -0,0 +1,8 @@
+# This is a sample Python script.
+n1 = int(input())
+if n1%10==0 or n1%10==5 or n1%10==6 or n1%10==7 or n1%10==8 or n1%10==9 or n1%10==11 or n1%100==12:
+    print (n1, 'программистов')
+elif n1%10==1:
+    print (n1, 'программист')
+elif n1%10==2 or n1%10==3 or n1%10==4:
+    print (n1, 'программиста')
